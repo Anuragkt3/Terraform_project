6f50618d20c7994ec8eb7054c56609854271ee37
Anurag <anuragkt3@gmail.com>
2023-05-27 10:36:22 +0530

    Terraform Main

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 resource "aws_instance" "app_server" {
   ami           = "ami-053b0d53c279acc90"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Terraform_Demo"
